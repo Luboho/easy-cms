4e2526bf9eb78a1b42815b4c461510001fc28cba
Luboho <testujem.php@gmail.com>
2020-09-04 13:46:34 +0200

    laravel/ui update

--- app/User.php
+++ app/User.php
@@ -38,11 +38,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new ResetPasswordNotification($token));
-    }
-
     public function post()
     {
         return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
